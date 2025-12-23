728ec68006a045dbfe4b6fd109d137c132828413
Фёдор <fendos2014@gmail.com>
2025-12-23 16:48:37 +0400

    Убрал лишний код, оставил в методах по сортировке имён и телефонов только сортировку без меню. В сортировке по id оставил меню для сортировки по возрастанию и четным.

--- src/main/java/userInterface/MenuManager.java
+++ src/main/java/userInterface/MenuManager.java
@@ -8,25 +8,26 @@ public class MenuManager implements Printable {
     private final Scanner scanner = new Scanner(System.in);
     private boolean running = true;
 
-    public void run(){
-        while (running){
+    public void run() {
+        while (running) {
             try {
                 printMainMenu();
                 String input = scanner.nextLine();
-                if (input.isEmpty()){
+                if (input.isEmpty()) {
                     System.out.println("Пожалуйста, выберите номер опции");
                     continue;
                 }
 
                 int mainOption = Integer.parseInt(input);
 
-                switch (mainOption){
+                switch (mainOption) {
                     case 1 -> printFillingDatabaseMenu();
                     case 2 -> printSortingMenu();
                     case 0 -> exitByChoice();
                     default -> System.out.println("Неверный выбор. Пожалуйста, выберите 1, 2 или 0");
                 }
-            } catch (Exception e){
+            }
+            catch (Exception e) {
                 System.out.println("Ошибка: " + e.getMessage());
                 System.out.println("Пожалуйста, введите корректный номер опции.");
             }
@@ -36,17 +37,17 @@ public class MenuManager implements Printable {
     @Override
     public void printFillingDatabaseMenu() {
         boolean backToMain = false;
-        while (!backToMain && running){
+        while (!backToMain && running) {
             Printable.super.printFillingDatabaseMenu();
             String input = scanner.nextLine().trim();
-            if (input.isEmpty()){
+            if (input.isEmpty()) {
                 System.out.println("Пожалуйста, введите номер опции.");
                 continue;
             }
             try {
                 int fillOption = Integer.parseInt(input);
 
-                switch (fillOption){
+                switch (fillOption) {
                     case 1 -> fillManually();
                     case 2 -> fillFromFile();
                     case 3 -> fillRandom();
@@ -56,7 +57,8 @@ public class MenuManager implements Printable {
                     }
                     default -> System.out.println("Неверный выбор. Пожалуйста, выберите от 0 до 3");
                 }
-            } catch (NumberFormatException e){
+            }
+            catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите цифру для выбора действия");
             }
         }
@@ -66,18 +68,18 @@ public class MenuManager implements Printable {
     @Override
     public void printSortingMenu() {
         boolean backToMain = false;
-        while (!backToMain && running){
+        while (!backToMain && running) {
             Printable.super.printSortingMenu();
             String input = scanner.nextLine().trim();
 
-            if (input.isEmpty()){
+            if (input.isEmpty()) {
                 System.out.println("Пожалуйста, введите номер опции.");
                 continue;
             }
             try {
                 int sortOption = Integer.parseInt(input);
 
-                switch (sortOption){
+                switch (sortOption) {
                     case 1 -> printDefaultOrder();
                     case 2 -> printNameSortingOptions();
                     case 3 -> printIdSortingOptions();
@@ -88,51 +90,21 @@ public class MenuManager implements Printable {
                     }
                     default -> System.out.println("Неверный выбор. Пожалуйста, выберите от 0 до 4");
                 }
-            } catch (NumberFormatException e){
-                System.out.println("Пожалуйста, введите цифру для выбора действия.");
-        }
-    }
-}
-
-    @Override
-    public void printNameSortingOptions() {
-        boolean backToMain = false;
-        while (!backToMain && running){
-            Printable.super.printNameSortingOptions();
-            String input = scanner.nextLine().trim();
-
-            if (input.isEmpty()){
-                System.out.println("Пожалуйста, введите номер опции.");
-                continue;
             }
-
-            try {
-                int nameSortOption = Integer.parseInt(input);
-
-                switch (nameSortOption){
-                    case 1 -> sortByNameAlphabet();
-                    case 2 -> sortByNameLength();
-                    case 3 -> sortByAmountVowelsOfName();
-                    case 0 -> {
-                        System.out.println("Назад...");
-                        backToMain = true;
-                    }
-                    default -> System.out.println("Неверный выбор. Пожалуйста, выберите от 0 до 3");
-                }
-            } catch (NumberFormatException e){
+            catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите цифру для выбора действия.");
+            }
         }
     }
-}
 
     @Override
     public void printIdSortingOptions() {
         boolean backToMain = false;
-        while (!backToMain && running){
+        while (!backToMain && running) {
             Printable.super.printIdSortingOptions();
             String input = scanner.nextLine().trim();
 
-            if (input.isEmpty()){
+            if (input.isEmpty()) {
                 System.out.println("Пожалуйста, введите номер опции.");
                 continue;
             }
@@ -140,97 +112,56 @@ public class MenuManager implements Printable {
             try {
                 int idSortOption = Integer.parseInt(input);
 
-                switch (idSortOption){
+                switch (idSortOption) {
                     case 1 -> sortIDByAscending();
-                    case 2 -> sortIDByDescending();
-                    case 3 -> sortIDByAmountOfNumbers();
+                    case 2 -> sortIDbyEven();
                     case 0 -> {
                         System.out.println("Назад...");
                         backToMain = true;
                     }
                     default -> System.out.println("Неверный выбор. Пожалуйста, выберите от 0 до 3");
                 }
-            } catch (NumberFormatException e){
+            }
+            catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите цифру для выбора действия.");
             }
         }
     }
 
     @Override
-    public void printPhoneSortingOptions() {
+    public void printNameSortingOptions() {
         boolean backToMain = false;
-        while (!backToMain && running){
-            Printable.super.printPhoneSortingOptions();
-            String input = scanner.nextLine().trim();
-
-            if (input.isEmpty()){
-                System.out.println("Пожалуйста, введите номер опции.");
-                continue;
-            }
-
-            try {
-                int phoneSortOption = Integer.parseInt(input);
-
-                switch (phoneSortOption){
-                    case 1 -> sortPhoneByNormalizedFormat();
-                    case 2 -> sortPhoneByCountryCode();
-                    case 3 -> sortPhoneByOperatorCode();
-                    case 0 -> {
-                        System.out.println("Назад...");
-                        backToMain = true;
-                    }
-                    default -> System.out.println("Неверный выбор. Пожалуйста, выберите от 0 до 3");
-                }
-            } catch (NumberFormatException e){
-                System.out.println("Пожалуйста, введите цифру для выбора действия.");
-            }
+        while (!backToMain && running) {
+            Printable.super.printNameSortingOptions();
+            backToMain = true;
         }
     }
 
-    private void sortByNameAlphabet(){
-        System.out.println("Клиенты отсортированы в алфавитном порядке имён");
+    @Override
+    public void printPhoneSortingOptions() {
+        boolean backToMain = false;
+        while (!backToMain && running) {
+            Printable.super.printPhoneSortingOptions();
+            backToMain = true;
+        }
     }
 
-    private void sortByNameLength(){
-        System.out.println("Клиенты отсортированы по длине имён");
-    }
-
-    private void sortByAmountVowelsOfName(){
-        System.out.println("Клиенты отсортированы по количеству гласных в имени");
-    }
-
-    private void sortIDByAscending(){
+    private void sortIDByAscending() {
         System.out.println("Клиенты отсортированы по возрастанию ID");
     }
 
-    private void sortIDByDescending(){
-        System.out.println("Клиенты отсортированы по убыванию ID");
+    private void sortIDbyEven() {
+        System.out.println("Клиенты отсортированы по четным ID");
     }
 
-    private void sortIDByAmountOfNumbers(){
-        System.out.println("Клиенты отсортированы по количеству цифр в ID");
-    }
-
-    private void sortPhoneByNormalizedFormat(){
-        System.out.println("Клиенты отсортированы по международному формату телефонного номера");
-    }
-
-    private void sortPhoneByCountryCode(){
-        System.out.println("Клиенты отсортированы по коду страны телефонного номера");
-    }
-
-    private void sortPhoneByOperatorCode(){
-        System.out.println("Клиенты отсортированы по коду оператора телефонного номера");
-    }
-
-    private void fillManually(){
+    private void fillManually() {
         System.out.println("Выбран способ ввода данных вручную");
         System.out.println("Введите данные...");
         System.out.println("Данные успешно сохранены");
 
     }
 
-    private void fillFromFile(){
+    private void fillFromFile() {
         System.out.println("Выбран способ ввода данных из файла\n");
         System.out.println("Введите путь к файлу: ");
         String filePath = scanner.nextLine().trim();
@@ -238,35 +169,35 @@ public class MenuManager implements Printable {
 
     }
 
-    private void fillRandom(){
+    private void fillRandom() {
         System.out.println("Выбран способ ввода случайных данных");
         int count = inputLengthOfValue("Сколько записей создать? ", 10);
         System.out.println("Создано: " + count + " записей");
     }
 
-    private void printDefaultOrder(){
+    private void printDefaultOrder() {
         System.out.println("Список клиентов");
     }
 
-    private void exitByChoice(){
+    private void exitByChoice() {
         System.out.println("Выход из программы...");
         running = false;
         scanner.close();
     }
 
-    private int inputLengthOfValue(String prompt, int defaultValue){
-        while (true){
+    private int inputLengthOfValue(String prompt, int defaultValue) {
+        while (true) {
             System.out.println(prompt + "(по умолчанию: " + defaultValue + "): ");
             String input = scanner.nextLine().trim();
 
-            if (input.isEmpty()){
+            if (input.isEmpty()) {
                 System.out.println("Значение по умолчанию: " + defaultValue);
                 return defaultValue;
             }
 
             try {
                 int value = Integer.parseInt(input);
-                if (value > 0){
+                if (value > 0) {
                     return value;
                 }
                 else if (value == 0) {
@@ -276,7 +207,8 @@ public class MenuManager implements Printable {
                 else {
                     System.out.println("Число должно быть положительным! Попробуйте еще раз.");
                 }
-            } catch (NumberFormatException e){
+            }
+            catch (NumberFormatException e) {
                 System.out.println("Пожалуйста, введите целое число! Попробуйте еще раз");
             }
         }
